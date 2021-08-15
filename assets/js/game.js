5501319315d9d9f2307c73804e33ea339f61fa39
Ant <huertinez.family@outlook.com>
2021-08-14 23:58:30 -0700

    starter js code

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of qotations marks aruond playerName
+console.log(playerName);
+console.log("This logs a string, good fo rleaving yourself a message");
+// this will do the math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight()
